7ae99fde56106ac390c08bc6f56626f6b3722cae
ABEDIER <abediernelson@live.fr>
2018-11-12 14:14:36 +0100

    refactor: FizzBuzzTest class name

--- src/test/java/FizzBuzzJ5Test.java
+++ src/test/java/FizzBuzzJ5Test.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class FizzBuzzTest {
+class FizzBuzzJ5Test {
 
     @BeforeAll
     static void testPrint(){
